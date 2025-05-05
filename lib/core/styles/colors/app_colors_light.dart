202076e7ffcad72feaf47ac1281c70fb1a7dafc4
Jakub Smolik <smolikja@protonmail.com>
2025-05-05 12:31:55 +0200

    feat: color styles

---
+++ lib/core/styles/colors/app_colors_light.dart
@@ -0,0 +1,17 @@
+import 'dart:ui';
+
+import 'package:portfolio_web/core/styles/colors/app_colors_scheme.dart';
+
+class AppColorsLight extends AppColorsScheme {
+  @override
+  Color get primary => const Color.fromARGB(255, 163, 81, 164);
+
+  @override
+  Color get secondary => const Color.fromARGB(255, 144, 212, 245);
+
+  @override
+  Color get onPrimary => const Color.fromARGB(255, 255, 255, 255);
+
+  @override
+  Color get onSecondary => const Color.fromARGB(255, 0, 0, 0);
+}
---
+++ lib/core/styles/colors/app_colors_scheme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColorsScheme {
+  Color get primary;
+
+  Color get secondary;
+
+  Color get onPrimary;
+
+  Color get onSecondary;
+}
