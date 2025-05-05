a13c8c05c0f1be661899e4c69fb429de7445e499
Jakub Smolik <smolikja@protonmail.com>
2025-05-05 12:36:14 +0200

    wip: text styles

---
+++ lib/core/styles/text_styles/text_styles.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class TextStyles {
+  /// fontSize - 13px, w500 weight
+  TextStyle example = const TextStyle(
+    fontSize: 13,
+    fontWeight: FontWeight.w500,
+  );
+}
