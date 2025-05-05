220c4db2f913d82ae98139671b6e6a091f751b28
Jakub Smolik <smolikja@protonmail.com>
2025-05-05 12:36:26 +0200

    feat: styles

---
+++ lib/core/styles/styles.dart
@@ -0,0 +1,9 @@
+import 'package:portfolio_web/core/styles/colors/app_colors.dart';
+import 'package:portfolio_web/core/styles/text_styles/text_styles.dart';
+
+class Styles {
+  Styles._();
+
+  static AppColors appColors = AppColors();
+  static TextStyles textStyles = TextStyles();
+}
